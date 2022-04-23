b4bb136d84717abab32142f988f06d4b14b4eea6
rizkyzhang <rizkyzhangdev@gmail.com>
2022-04-23 15:30:41 +0700

    feat: initial commit

---
+++ config/plugins.js
@@ -0,0 +1,25 @@
+module.exports = ({ env }) => ({
+  slugify: {
+    enabled: true,
+    config: {
+      contentTypes: {
+        article: {
+          field: "slug",
+          references: "title",
+        },
+      },
+    },
+  },
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-supabase",
+      providerOptions: {
+        apiUrl: env("SUPABASE_API_URL"),
+        apiKey: env("SUPABASE_API_KEY"),
+        bucket: env("SUPABASE_BUCKET"),
+        directory: env("SUPABASE_DIRECTORY"),
+        options: {},
+      },
+    },
+  },
+});
